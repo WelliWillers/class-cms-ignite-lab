3fcf67143ce6d56d54ca2eea241024e84a7c547c
WelliWillers <wellington.willers@gmail.com>
2022-06-23 23:49:17 -0300

    Tela da home pronta e funcional; ajustes de style no evento

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o4t8wh0k3101xx21wp8zku/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
